a22422da376c2306a9b895c6cada236b88ba66ca
Sajal Batra <sajalbatra7@gmail.com>
2024-07-28 19:01:17 +0530

    Sign in option fixed

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -40,7 +40,7 @@ const Header = () => {
                         onClick={() => signIn()}
                         className="bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4 rounded"
                     >
-                        Sign In
+                        Sign In-To Save Post
                     </button>
 
                 )}
